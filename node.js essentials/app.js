baf060332280bc70bc9bfad216a20421e075e0a8
Kieran Dunch <dunch.kieran@gmail.com>
2024-03-01 07:27:06 -0800

    node additions

--- node.js essentials/app.js
+++ node.js essentials/app.js
@@ -3,6 +3,7 @@
 // a very common pattern in programming and is used in many different languages and
 // frameworks.
 
+// Events module
 
 // Here we require in the 'events' module and save a reference to it in an events variable
 let events = require('events');
@@ -20,3 +21,20 @@ myEmitter.on('celebration', listenerCallback);
 
 // We can use the emit method to trigger the event and run the callback function
 myEmitter.emit('celebration', 'birthday');
+
+//  User Input/Output
+
+// stdout is a writable stream that we can use to output data to the console
+// write method is used to write data to the console
+process.stdout.write("I'm thinking of a number from 1 through 10. What do you think it is? \n(Write \"quit\" to give up.)\n\nIs the number ... ");
+
+let playGame = (userInput) => {
+  let input = userInput.toString().trim();
+  testNumber(input);
+};
+
+// stdin is a readable stream that we can use to read data from the console
+// on method is used to listen for a specific event and then run a callback function when that event occurs
+// in this case we are listening for the 'data' event which occurs when the user enters input
+// when the 'data' event occurs, we run the playGame function
+process.stdin.on('data', playGame);
