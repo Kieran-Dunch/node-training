b7684c49085792ed1e15a5de431ad257ca1862ce
Kieran Dunch <dunch.kieran@gmail.com>
2024-10-22 11:48:41 -0600

    save template

---
+++ quotekeeper/user-visits-root-test.js
@@ -0,0 +1,16 @@
+const { assert } = require('chai');
+
+// The quote itself
+// Who the quote is attributed to
+// The source of the quote:
+// Book
+// Article
+// Speech
+// IRL
+
+describe('User visits root', () => {
+  describe('Posting a quote', () => {
+    it('saves quote and metadata submitted by user', () => {
+    });
+  });
+});
