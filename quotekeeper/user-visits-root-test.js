145bb1291f2f6847ef1ba6302381b157205aef56
Kieran Dunch <dunch.kieran@gmail.com>
2024-10-22 11:53:28 -0600

    setup test

--- quotekeeper/user-visits-root-test.js
+++ quotekeeper/user-visits-root-test.js
@@ -11,6 +11,16 @@ const { assert } = require('chai');
 describe('User visits root', () => {
   describe('Posting a quote', () => {
     it('saves quote and metadata submitted by user', () => {
+      const quote = 'feature tests often hit every level of the TDD Testing Pyramid';
+      const author = "username";
+      const source = "Book";
+
+      browser.url('/');
+
+      browser.setValue('textarea[id=quote]', quote);
+      browser.setValue('input[id=author]', author);
+      browser.setValue('input[id=source]', source);
+      browser.click('input[type=submit]');
     });
   });
 });
