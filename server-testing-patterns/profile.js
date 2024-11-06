bfee2520af121fb62a44911867c214ab67080fad
Kieran Dunch <dunch.kieran@gmail.com>
2024-11-06 10:09:25 -0800

    handlebars

--- server-testing-patterns/profile.js
+++ server-testing-patterns/profile.js
@@ -2,12 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/alice', (req, res) => {
-  res.send('<h1 id="welcome-message">Welcome alice!</h1>');
-});
-
-router.get('/bob', (req, res) => {
-  res.send('<h1 id="welcome-message">Welcome bob!</h1>');
+router.get('/:username', (req, res) => {
+  const username = req.params.username;
+  res.render('profile', { username });
 });
 
 module.exports = router;
